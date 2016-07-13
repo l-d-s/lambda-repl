afc351a0d3cd31c2cbb78b5644784d737de4d6f1
l-d-s <l@sdf.org>
2016-07-13 16:56:08 +1000

    Add evaluation printer.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,22 @@
 module Main where
 
 import LambdaCalculus.Terms
+import LambdaCalculus.ExampleTerms
+import LambdaCalculus.Conversions
+
+
+takeWhileDistinct :: Eq a => [a] -> [a]
+takeWhileDistinct [] = []
+takeWhileDistinct [x] = [x]
+takeWhileDistinct [x, y] = if x == y then [x] else [x, y]
+takeWhileDistinct (x:y:zs) =
+    if x == y then [x] else x : takeWhileDistinct (y:zs)
+
+
+printEvaluation :: (LambdaTerm -> LambdaTerm) -> LambdaTerm -> IO ()
+printEvaluation evaluator lt =
+    mapM_ print (takeWhileDistinct (iterate evaluator lt))
+
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
